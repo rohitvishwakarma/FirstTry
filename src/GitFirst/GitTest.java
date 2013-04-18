2c5a90ee658a3582de654f8d1878590d06bac47b
rohit vishwakarma <rohit4ualways@gmail.com>
2013-04-18 14:25:50 +0530

    second one

--- src/GitFirst/GitTest.java
+++ src/GitFirst/GitTest.java
@@ -3,5 +3,6 @@ package GitFirst;
 public class GitTest {
 	  public static void main(String[] args) {
 	    System.out.println("Git is fun");
+	    System.out.println("Version 2 Git is fun");
 	  }
 	} 
